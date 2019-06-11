b7e3f94c20112d5c84c047b1afbdf06badbb575c
Olivier ESUKA <oesukam@gmail.com>
2019-06-11 21:58:28 +0200

    chore: setup test
    - eject create-react-app
    - add test for App.js
    - delete useless files
    - add snapshots to .gitignore
    
    [Finishes #166617782]
    
    chore: add pull request template

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../../components/App';
 
-it('renders without crashing', () => {
+it('should render without crashing', () => {
   shallow(<App />);
 });
---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h3>React App</h3>
+    </React.Fragment>
+  );
+}
+
+export default App;
